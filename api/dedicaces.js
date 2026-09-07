43c72b2a5372a9a9385445a80f405f14b7f7b66c
freddydelafosse-droid <freddy.delafosse@gmail.com>
2026-09-07 18:06:08 +0200

    Create dedicaces.js

---
+++ api/dedicaces.js
@@ -0,0 +1,13 @@
+export default function handler(req, res) {
+  if (req.method !== "GET") {
+    return res.status(405).json({ error: "Méthode non autorisée" });
+  }
+
+  const dedicaces = [];
+
+  res.setHeader("Cache-Control", "no-store");
+
+  return res.status(200).json({
+    dedicaces
+  });
+}
