b0d5ead7d760932fffe714e7e64f84e8954f5be7
freddydelafosse-droid <freddy.delafosse@gmail.com>
2026-09-10 17:27:33 +0200

    Update [station].js

--- api/nowplaying/[station].js
+++ api/nowplaying/[station].js
@@ -3,7 +3,7 @@ export default async function handler(req, res) {
 
   try {
     const response = await fetch(
-      `http://82.66.219.41/api/nowplaying/${station}`,
+      `https://radio.technorizon.fr/api/nowplaying/${station}`,
       { cache: "no-store" }
     );
 
