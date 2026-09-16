3b1b8ed6d9c3c20ba19f53d3a33afbbd70af0bcf
freddydelafosse-droid <freddy.delafosse@gmail.com>
2026-09-16 18:21:41 +0200

    Refresh PWA cache after language isolation

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='technorizon-v2-shell-4';
-const SHELL=['/','/index.html','/style-v2.css?v=13','/ImageLogoFinal.png','/Fond-Technorizon-V2.png.png','/jeux.html','/jeux.css?v=2','/jeux.js?v=5','/jeux-data.js?v=1'];
+const CACHE='technorizon-v2-shell-5';
+const SHELL=['/','/index.html','/style-v2.css?v=13','/ImageLogoFinal.png','/Fond-Technorizon-V2.png.png','/jeux.html','/jeux.css?v=2','/jeux.js?v=7','/jeux-data.js?v=1'];
 
 self.addEventListener('install',event=>{
   self.skipWaiting();
