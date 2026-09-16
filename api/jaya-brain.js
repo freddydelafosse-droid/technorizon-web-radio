0707c035fd1a4cfb392c068644e8a948c4a951d4
freddydelafosse-droid <freddy.delafosse@gmail.com>
2026-09-16 17:37:33 +0200

    Recognize feminine Technorizon rotation questions

--- api/jaya-brain.js
+++ api/jaya-brain.js
@@ -326,7 +326,7 @@ if (asksTechnorizon) {
 
 if (artistMatch) {
   const asksArtistTechnorizon =
-    /\\b(technorizon|rotation|diffuse|diffusé|programme|bibliotheque)\\b/i.test(cleanQuestion);
+    /\\b(technorizon|rotation|diffuse|diffusee|diffusé|diffusée|programme|programmee|programmée|bibliotheque)\\b/i.test(cleanQuestion);
 
   const hasArtistProfile = Boolean(
     artistMatch.biography ||
