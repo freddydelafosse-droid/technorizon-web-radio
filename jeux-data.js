d44919f112c264ece951a6df098bfe8e1a77f594
freddydelafosse-droid <freddy.delafosse@gmail.com>
2026-09-16 12:37:15 +0200

    Ajoute le catalogue étendu de la Game Zone

---
+++ jeux-data.js
@@ -0,0 +1,221 @@
+window.TECHNORIZON_TRACKS = `
+Technotronic|Pump Up the Jam
+SNAP!|The Power
+SNAP!|Rhythm Is a Dancer
+2 Unlimited|Get Ready for This
+2 Unlimited|No Limit
+Haddaway|What Is Love
+Culture Beat|Mr. Vain
+Corona|The Rhythm of the Night
+Gala|Freed From Desire
+La Bouche|Be My Lover
+Real McCoy|Another Night
+Dr. Alban|It's My Life
+Dr. Alban|Sing Hallelujah!
+Ice MC|Think About the Way
+Whigfield|Saturday Night
+Captain Hollywood Project|More and More
+Maxx|Get-A-Way
+Masterboy|Feel the Heat of the Night
+Twenty 4 Seven|Slave to the Music
+Cappella|U Got 2 Let the Music
+Ace of Base|All That She Wants
+Scatman John|Scatman
+Rednex|Cotton Eye Joe
+E-Type|Set the World on Fire
+DJ BoBo|Everybody
+Fun Factory|Close to You
+Le Click|Tonight Is the Night
+Rozalla|Everybody's Free
+Crystal Waters|Gypsy Woman
+Robin S|Show Me Love
+CeCe Peniston|Finally
+Black Box|Ride on Time
+C+C Music Factory|Gonna Make You Sweat
+The KLF|3 A.M. Eternal
+M People|Moving on Up
+Livin' Joy|Dreamer
+Alex Party|Don't Give Me Your Life
+Nightcrawlers|Push the Feeling On
+Ultra Naté|Free
+Everything But The Girl|Missing
+Robert Miles|Children
+Sash!|Ecuador
+Sash!|Encore une fois
+Faithless|Insomnia
+Darude|Sandstorm
+ATB|9 PM (Till I Come)
+Alice Deejay|Better Off Alone
+Eiffel 65|Blue (Da Ba Dee)
+Gigi D'Agostino|L'Amour Toujours
+Vengaboys|Boom, Boom, Boom, Boom!!
+Aqua|Barbie Girl
+DJ Dado|X-Files
+Chicane|Saltwater
+Paul van Dyk|For an Angel
+Binary Finary|1998
+Energy 52|Café del Mar
+Delerium|Silence
+Safri Duo|Played-A-Live
+Ian Van Dahl|Castles in the Sky
+Lasgo|Something
+Modjo|Lady (Hear Me Tonight)
+Stardust|Music Sounds Better with You
+Daft Punk|One More Time
+Daft Punk|Harder, Better, Faster, Stronger
+Alcazar|Crying at the Discoteque
+Sophie Ellis-Bextor|Murder on the Dancefloor
+Kylie Minogue|Can't Get You Out of My Head
+Benny Benassi|Satisfaction
+Eric Prydz|Call on Me
+Room 5|Make Luv
+Junior Senior|Move Your Feet
+Bob Sinclar|Love Generation
+Bob Sinclar|World, Hold On
+David Guetta|Love Don't Let Me Go
+David Guetta|The World Is Mine
+Pakito|Living on Video
+Global Deejays|The Sound of San Francisco
+Royal Gigolos|California Dreamin'
+Michael Gray|The Weekend
+Mylo|Drop the Pressure
+Freemasons|Love on My Mind
+Roger Sanchez|Another Chance
+Basement Jaxx|Where's Your Head At
+The Shapeshifters|Lola's Theme
+Tomcraft|Loneliness
+Fedde Le Grand|Put Your Hands Up for Detroit
+Mason|Exceeder
+Ida Corr|Let Me Think About It
+Guru Josh Project|Infinity 2008
+Laurent Wolf|No Stress
+Martin Solveig|C'est la vie
+Martin Solveig|Hello
+Yves Larock|Rise Up
+September|Cry for You
+Cascada|Everytime We Touch
+Cascada|Evacuate the Dancefloor
+Basshunter|Now You're Gone
+Scooter|The Logical Song
+Special D.|Come with Me
+Groove Coverage|Moonlight Shadow
+Sylver|Turn the Tide
+Milk Inc.|Walk on Water
+Kate Ryan|Désenchantée
+Kate Ryan|Ella elle l'a
+David Guetta|When Love Takes Over
+David Guetta|Sexy Bitch
+Edward Maya|Stereo Love
+Inna|Hot
+Alexandra Stan|Mr. Saxobeat
+Yolanda Be Cool|We No Speak Americano
+Stromae|Alors on danse
+Duck Sauce|Barbra Streisand
+Swedish House Mafia|One
+Eric Prydz|Pjanoo
+deadmau5|Ghosts 'n' Stuff
+Calvin Harris|I'm Not Alone
+Justice|D.A.N.C.E.
+The Chemical Brothers|Galvanize
+The Prodigy|Omen
+Tiësto|Adagio for Strings
+Avicii|Levels
+Avicii|Wake Me Up
+Swedish House Mafia|Don't You Worry Child
+Calvin Harris|Feel So Close
+Calvin Harris|Summer
+David Guetta|Titanium
+David Guetta|Play Hard
+Martin Garrix|Animals
+Major Lazer|Lean On
+DJ Snake|Turn Down for What
+DJ Snake|Taki Taki
+Clean Bandit|Rather Be
+Robin Schulz|Prayer in C
+Robin Schulz|Sugar
+Lost Frequencies|Are You with Me
+Kygo|Firestone
+Felix Jaehn|Ain't Nobody
+Kungs|This Girl
+Ofenbach|Be Mine
+Feder|Goodbye
+The Chainsmokers|Closer
+Zedd|Clarity
+Zedd|Stay
+Marshmello|Happier
+Alan Walker|Faded
+Galantis|Runaway (U & I)
+Duke Dumont|I Got U
+Disclosure|Latch
+Route 94|My Love
+Gorgon City|Ready for Your Love
+Klingande|Jubel
+Bakermat|One Day (Vandaag)
+Klingande|Riva
+Sigala|Easy Love
+Jonas Blue|Fast Car
+Jax Jones|You Don't Know Me
+Regard|Ride It
+Meduza|Piece of Your Heart
+Dynoro|In My Mind
+Riton|Turn Me On
+Purple Disco Machine|Hypnotized
+Topic|Breaking Me
+Joel Corry|Head & Heart
+Calvin Harris|How Deep Is Your Love
+Disclosure|You & Me
+Flume|Never Be Like You
+Major Lazer|Light It Up
+Martin Solveig|Intoxicated
+David Guetta|Hey Mama
+Tiësto|Red Lights
+Hardwell|Spaceman
+Armin van Buuren|This Is What It Feels Like
+Dimitri Vegas & Like Mike|Tremor
+DVBBS|Tsunami
+R3HAB|All Around the World (La La La)
+Axwell Λ Ingrosso|More Than You Know
+Alesso|Heroes (We Could Be)
+Sebastian Ingrosso|Reload
+Nicky Romero|Toulouse
+Showtek|Booyah
+Dua Lipa|Physical
+Dua Lipa|Don't Start Now
+The Weeknd|Blinding Lights
+David Guetta & Bebe Rexha|I'm Good (Blue)
+Elton John & Dua Lipa|Cold Heart (PNAU Remix)
+Purple Disco Machine|Dopamine
+Calvin Harris & Ellie Goulding|Miracle
+David Guetta & OneRepublic|I Don't Wanna Wait
+Peggy Gou|(It Goes Like) Nanana
+Kenya Grace|Strangers
+cassö|Prada
+Tiësto|The Business
+Joel Corry|Sorry
+Nathan Dawe|21 Reasons
+LF SYSTEM|Afraid to Feel
+Fred again..|Marea (We've Lost Dancing)
+John Summit|Where You Are
+Swedish House Mafia|Moth to a Flame
+Alok|Hear Me Now
+Lost Frequencies|Where Are You Now
+Becky Hill|Remember
+Becky Hill|Disconnect
+Joel Corry|Lonely
+Topic & A7S|Kernkraft 400 (A Better Day)
+Gabry Ponte|Thunder
+RAYE|Regardless
+Sigala|Melody
+Jax Jones|Where Did You Go?
+MEDUZA|Lose Control
+Purple Disco Machine|In the Dark
+Ofenbach|Head Shoulders Knees & Toes
+Kungs|Never Going Home
+Robin Schulz|Young Right Now
+Alesso & Katy Perry|When I'm Gone
+Tiësto & Ava Max|The Motto
+David Guetta & Kim Petras|When We Were Young (The Logical Song)
+`.trim().split('\n').map((line, index) => {
+  const separator = line.indexOf('|');
+  return { id: index + 1, artist: line.slice(0, separator), title: line.slice(separator + 1) };
+});
